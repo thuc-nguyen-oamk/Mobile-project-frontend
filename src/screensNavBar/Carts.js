f656af2dc2f8b7c1b75c3001afa850f25b52c0e0
Hieu Nguyen <nth937@gmail.com>
2022-03-25 20:07:18 +0200

    navigation / screens

---
+++ src/screensNavBar/Carts.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Carts = () => {
+  return (
+    <View>
+      <Text styles={{color: 'red'}}>Carts</Text>
+    </View>
+  )
+}
+
+export default Carts
