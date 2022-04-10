380321bb5f158fabed5494889e74f64cf31b3be1
TramVu <71011294+TramVu@users.noreply.github.com>
2022-04-10 18:44:51 +0300

    create user profile+detail

--- android/app/src/main/java/com/titi/MainApplication.java
+++ android/app/src/main/java/com/titi/MainApplication.java
@@ -10,7 +10,9 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
+import java.util.List; 
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +36,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
+      }
       };
 
   @Override
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,13 @@
 import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
 import axios from 'react-native-axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { useNavigation } from '@react-navigation/native';
 
 const UserProfile = (props) => {
+
+  const navigation = useNavigation()
+  
   const logOut = () => {
     const removeToken = async () => {
       try {
@@ -19,20 +23,85 @@ const UserProfile = (props) => {
     props.setIsLoggedIn(false);
   }
   return (
-    <View>
-      <Text style={styles.text}>UserProfile</Text>
-
-      <Button title="Logout" color="gray"
-        onPress={logOut}
-      />
-    </View>
-  )
+    <View style={styles.container}>
+  
+        <TouchableOpacity style={styles.header}>
+          <View style={styles.left}>
+            <Image style={styles.img} source={{uri: "https://i.imgur.com/a5piz6z.png"}} />
+          </View>
+          <View style={styles.right}>
+            <Text style={{fontWeight: "bold", fontSize:20}}>Welcome USER</Text>
+            <View style={{backgroundColor:"#fff", padding:2, borderRadius:2}}>
+              <Button title="Logout" color="#fb70ff" borderColor= "#fff" onPress={logOut} />
+            </View>
+          </View>
+        </TouchableOpacity>
+  
+        
+        <TouchableOpacity style={styles.row}>         
+            <Icon name="cart-outline" style={styles.icon} />
+            <Text style={styles.text}>My orders history</Text>          
+        </TouchableOpacity>
+  
+        <TouchableOpacity style={styles.row}
+                          onPress ={() => {navigation.navigate ('EditProfile')}}
+        >
+            <Icon name="person-circle-outline" style={styles.icon} />  
+            <Text style={styles.text}>Edit</Text>   
+        </TouchableOpacity>
+  
+        <TouchableOpacity style={styles.row}>
+            <Icon name="help-circle-outline" style={styles.icon} />
+            <Text style={styles.text}>Help center</Text>
+        </TouchableOpacity>
+      </View>
+     
+    )
 }
-
+  
+export default UserProfile; 
+  
 const styles = StyleSheet.create({
   text: {
-    color: "black"
+    color: 'black',
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
   },
-})
+  header: {
+    flexDirection: 'row',
+    backgroundColor: '#fb70ff',
+  },
+  left: {
+    padding: 20,
+  },
+  img: {
+    width: 100,
+    height: 100,
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
 
-export default UserProfile
+  row: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: '80%',
+    height: 80,
+    padding: 20,
+    paddingBottom: 20,
+    marginTop: 15,
+  },
+  icon: {
+    color: 'blue',
+    fontSize: 34,
+  },
+});
+
+
