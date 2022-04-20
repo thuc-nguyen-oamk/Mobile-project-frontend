3cb18d048f9f121ab7d5454d9673d2322a158666
TramVu <71011294+TramVu@users.noreply.github.com>
2022-04-20 14:20:08 +0300

    Adjusting css

--- src/screensNavBar/Carts.js
+++ src/screensNavBar/Carts.js
@@ -1,37 +1,46 @@
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
-import { useState, useEffect } from "react";
+import {useState, useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import CartsItem from "../components/CartsItem";
+import CartsItem from '../components/CartsItem';
 
 const Carts = () => {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const readStorage = async () => {
       try {
-        const cartString = await AsyncStorage.getItem('cart')
-        let _cart = JSON.parse(cartString)
+        const cartString = await AsyncStorage.getItem('cart');
+        let _cart = JSON.parse(cartString);
         // console.log('read x', _cart, typeof _cart);
-        if (!_cart) {_cart = []}
-        setCart(_cart)
+        if (!_cart) {
+          _cart = [];
+        }
+        setCart(_cart);
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
-    }
-    readStorage()
-  }, [cart])
+    };
+    readStorage();
+  }, [cart]);
 
   const deleteItem = async (id, detail_id) => {
     try {
       let currentIndex = cart.findIndex(function (p, i) {
-        return (p.product_id === id && p.product_detail_id === detail_id)
-      })
-      cart.splice(currentIndex, 1)
-      await AsyncStorage.setItem('cart', JSON.stringify(cart))
-      setCart([...cart])
+        return p.product_id === id && p.product_detail_id === detail_id;
+      });
+      cart.splice(currentIndex, 1);
+      await AsyncStorage.setItem('cart', JSON.stringify(cart));
+      setCart([...cart]);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   };
 
@@ -41,70 +50,140 @@ const Carts = () => {
       //   (p.product_id === id && p.product_detail_id === detail_id)
       // });
       let currentIndex = cart.findIndex(function (p, i) {
-        return (p.product_id === id && p.product_detail_id === detail_id)
+        return p.product_id === id && p.product_detail_id === detail_id;
       });
-      cart[currentIndex].product_qty -= (cart[currentIndex].product_qty === 0) ? 0 : 1
-      await AsyncStorage.setItem('cart', JSON.stringify(cart))
-      setCart([...cart])
+      cart[currentIndex].product_qty -=
+        cart[currentIndex].product_qty === 0 ? 0 : 1;
+      await AsyncStorage.setItem('cart', JSON.stringify(cart));
+      setCart([...cart]);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   };
 
   const upQty = async (id, detail_id) => {
     try {
       let currentIndex = cart.findIndex(function (p, i) {
-        return (p.product_id === id && p.product_detail_id === detail_id)
-      })
-      cart[currentIndex].product_qty += 1
-      await AsyncStorage.setItem('cart', JSON.stringify(cart))
-      setCart([...cart])
+        return p.product_id === id && p.product_detail_id === detail_id;
+      });
+      cart[currentIndex].product_qty += 1;
+      await AsyncStorage.setItem('cart', JSON.stringify(cart));
+      setCart([...cart]);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   };
 
   const getTotalPrice = () => {
-    if (cart.length === 0) return 0
-    if (cart.length === 1) return cart[0].product_price_discounted * cart[0].product_qty
+    if (cart.length === 0) return 0;
+    if (cart.length === 1)
+      return cart[0].product_price_discounted * cart[0].product_qty;
     const totalObj = cart.reduce((prev, curr) => ({
-      price: curr.product_price_discounted * curr.product_qty + prev.product_price_discounted * prev.product_qty,
+      price:
+        curr.product_price_discounted * curr.product_qty +
+        prev.product_price_discounted * prev.product_qty,
       qty: 1,
     }));
-    return totalObj.price
+    return totalObj.price;
   };
 
   const checkOut = () => {
-    console.log(cart)
-  }
+    console.log(cart);
+  };
+
+  const footer = () => {
+    return (
+      <View>
+        <Text style={styles.text}>Total: {getTotalPrice()}$</Text>
+      </View>
+    );
+  };
+
+  const ItemDivider = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '100%',
+          backgroundColor: '#607D8B',
+        }}
+      />
+    );
+  };
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={cart}
-        renderItem={({ item }) => 
+        renderItem={({item}) => (
           <View style={styles.wrap}>
-            <CartsItem item={item} 
-              deleteItem={() => deleteItem(item.product_id, item.product_detail_id)}
+            <CartsItem
+              item={item}
+              deleteItem={() =>
+                deleteItem(item.product_id, item.product_detail_id)
+              }
               downQty={() => downQty(item.product_id, item.product_detail_id)}
               upQty={() => upQty(item.product_id, item.product_detail_id)}
             />
           </View>
+        )}
+        keyExtractor={item =>
+          `${item.product_id} and ${item.product_detail_id}`
         }
-        keyExtractor={item => `${item.product_id} and ${item.product_detail_id}`}
+        ItemSeparatorComponent={ItemDivider}
+        ListFooterComponent={footer}
       />
-      <View style={styles.bottom}>
-        <Text style={styles.text}>Total: {getTotalPrice()}$</Text>
-        <Button title="Checkout" color="#f48cff" onPress={checkOut}/>
+      <View style={styles.footerStyle}>
+        <TouchableOpacity style={styles.footer} onPress={checkOut}>
+          <Text style={styles.footerText}>CHECKOUT</Text>
+        </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Carts
+export default Carts;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  wrap: {
+    backgroundColor: '#ebf3fa',
+    height: 120,
+    justifyContent: 'center',
+    paddingTop: 10,
+    flex: 3,
+  },
   text: {
     color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+    left: '75%',
   },
-})
+  footerStyle: {
+    flexDirection: 'column',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  footer: {
+    backgroundColor: '#ffa64d',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+  footerText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
